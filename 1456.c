fc5440dd87be39b506c353bf604f5223553e270e
throne-of-sanity <zhang3502193449@outlook.com>
2026-01-22 18:53:47 +0800

    first commit

---
+++ 1456.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#define MAX(a, b) ((b) > (a) ? (b) : (a))
+int maxVowels(char *s, int k)
+{
+    int ans = 0, vowel = 0;
+    for (int i = 0; s[i]; i++)
+    {
+        if (s[i] == 'a' || s[i] == 'e' || s[i] == 'i' || s[i] == 'o' || s[i] == 'u')
+        {
+            vowel++;
+        }
+        int left = i - k + 1;
+        if (left < 0)
+        {
+            continue;
+        }
+        ans = MAX(ans, vowel);
+        if (ans == k)
+        {
+            break;
+        }
+        char out = s[left];
+        if (out == 'a' || out == 'e' || out == 'i' || out == 'o' || out == 'u')
+        {
+            vowel--;
+        }
+    }
+    return ans;
+}
+int main()
+{
+    char s[] = "abciiidef";
+    int k = 3;
+    int res = maxVowels(s, k);
+    printf("%d\n", res);
+    return 0;
+}
